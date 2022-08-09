aa9aa8551a66c291bfc798ba20c02e8a5f840235
Dannel Albert <cuebix@gmail.com>
2022-08-08 20:57:38 -0400

    feat: add console log to confirm vercel adapter use

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vercel from 'solid-start-vercel'
 let adapter: ReturnType<typeof vercel>
 
 if (process.env.CLOUD_SERVICE_NAME === 'vercel')  {
+  console.log('Using vercel adapter...')
   adapter = vercel()
 }
 
